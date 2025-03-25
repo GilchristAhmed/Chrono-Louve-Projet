ea311c8b5baeb8ae831af39d6a14d926812925a8
Scotty-lord <105054@ecole-it.com>
2025-03-25 14:25:38 +0100

    Update eventRoutes.js

--- backend/routes/eventRoutes.js
+++ backend/routes/eventRoutes.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 
-router.get('/events/:id', eventController.getEvent);
+router.get('/events/:id', eventController.getEventById);
 router.post('/events', eventController.createEvent);
 
 module.exports = router;
