6ba39f6a1020caf88ec2d701ffc20647aa64d3e0
Yan <wambeyan@gmail.com>
2025-04-24 01:07:31 +0200

    Gestion commentaire Mod

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {RegisterComponent} from './pages/register/register.component';
 import {LoginComponent} from './pages/login/login.component';
+import { CommentformComponent } from './pages/commentform/commentform.component';
 
 export const routes: Routes = [
   {path:'',component:RegisterComponent},
   {path:'login',component:LoginComponent},
+  {path:'comment',component:CommentformComponent},
 ];
