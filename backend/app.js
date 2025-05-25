cc836e7c72078ef4bcb9ce569b53eaf774ce447f
Gilchrist Moluh Majid <68822147+GilchristAhmed@users.noreply.github.com>
2025-05-25 13:17:17 +0200

    update

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-//const eventRoutes = require('./routes/eventRoutes');
-const figureRoutes = require('./routes/figureRoutes');
-const userRoutes = require('./routes/userRoutes');
+
+const Routes = require('./routes');
 
 const app = express();
 dotenv.config();
@@ -13,9 +12,9 @@ app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-//app.use('/api/event', eventRoutes);
-app.use('/api/figure', figureRoutes);
-app.use('/user', userRoutes);
+
+
+app.use('/api', Routes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
